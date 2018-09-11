434cfc83ab6a4ff5f2e30d0e847ddec69e6d9996
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2018-09-11 13:11:29 +0200

    Added AuthenticationEntryPoint for 403.

---
+++ src/main/java/com/chumbok/security/Http403ForbiddenEntryPoint.java
@@ -0,0 +1,32 @@
+package com.chumbok.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * AuthenticationEntryPoint for HTTP 403.
+ */
+@Slf4j
+public class Http403ForbiddenEntryPoint implements AuthenticationEntryPoint {
+
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+            throws IOException, ServletException {
+
+        log.debug("Pre-authenticated entry point called. Rejecting access");
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+
+        response.getOutputStream()
+                .println("{ \"code\": \"FORBIDDEN_REQUEST\", \"message\": \"Permission denied for the resource.\" }");
+
+    }
+}
