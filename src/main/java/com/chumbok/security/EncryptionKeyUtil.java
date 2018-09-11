2138a8012b919527e3cc3db8c2ad581a7b806ef1
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2018-09-11 14:40:02 +0200

    Added load private key method in encryption util.

--- src/main/java/com/chumbok/security/EncryptionKeyUtil.java
+++ src/main/java/com/chumbok/security/EncryptionKeyUtil.java
@@ -1,9 +1,11 @@
 package com.chumbok.security;
 
+import sun.security.rsa.RSAPrivateCrtKeyImpl;
 import sun.security.rsa.RSAPublicKeyImpl;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 
 /**
@@ -23,6 +25,22 @@ public class EncryptionKeyUtil {
             return new RSAPublicKeyImpl(keyBytes);
         } catch (Exception ex) {
             throw new RuntimeException("Could not load PublicKey from " + path, ex);
+        }
+    }
+
+    /**
+     * Load private key from file path.
+     *
+     * @param path
+     * @return
+     * @throws RuntimeException in case any exception occurs in the process.
+     */
+    public PrivateKey loadPrivateKey(String path) {
+        try {
+            byte[] keyBytes = Files.readAllBytes(Paths.get(path));
+            return RSAPrivateCrtKeyImpl.newKey(keyBytes);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not load PrivateKey from " + path, ex);
         }
     }
 
